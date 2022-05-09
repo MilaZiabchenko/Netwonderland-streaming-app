7a5957dff5eb702aa2eb6b26d2768ade643dd739
Mila Ziabchenko <mi.podgurska@gmail.com>
2022-05-09 11:51:36 +0300

    modified:   src/components/Search/Search.css 	modified:   src/components/Search/Search.js

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,24 +3,18 @@ import './Search.css';
 const Search = ({ inputText, handleChange, clearInput }) => {
   return (
     <section className='search'>
-      <div className='search-inputs'>
-        <input
-          type='text'
-          value={inputText}
-          onChange={handleChange}
-          placeholder='Search...'
-          autoFocus
-        />
-        <div className='search-icon'>
-          {inputText === '' ? (
-            <i className='fas fa-search fa-2x'></i>
-          ) : (
-            <i
-              className='fas fa-times fa-2x clear-btn'
-              onClick={clearInput}
-            ></i>
-          )}
-        </div>
+      <input
+        type='text'
+        value={inputText}
+        onChange={handleChange}
+        placeholder='Search...'
+      />
+      <div className='search-icon'>
+        {inputText === '' ? (
+          <i className='fas fa-search fa-2x'></i>
+        ) : (
+          <i className='fas fa-times fa-2x clear-btn' onClick={clearInput}></i>
+        )}
       </div>
     </section>
   );
